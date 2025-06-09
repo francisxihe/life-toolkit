29e120153ea0a8a53eb5b6eeb9105e2c5a60e4ef
xuwh <xuwenhua@XWH-MacBook-Pro.local>
2025-06-09 12:41:27 +0800

    feat: style问题

--- packages/components-web/ui/src/FlexibleContainer/Container.tsx
+++ packages/components-web/ui/src/FlexibleContainer/Container.tsx
@@ -1,21 +1,21 @@
-import clsx from 'clsx';
-import styles from './style.module.less';
+import clsx from "clsx";
+import styles from "./style.module.less";
 
 type ContainerProps = {
-  direction?: 'vertical' | 'horizontal';
+  direction?: "vertical" | "horizontal";
   className?: string;
   children: React.ReactNode;
 } & React.HTMLAttributes<HTMLDivElement>;
 
 export function Container(props: ContainerProps) {
-  const { direction = 'horizontal', className, children, ...rest } = props;
+  const { direction = "horizontal", className, children, ...rest } = props;
   return (
     <div
       className={clsx(
-        styles['container'],
-        direction === 'vertical' ? styles['container-vertical'] : '',
-        direction === 'horizontal' ? styles['container-horizontal'] : '',
-        className ?? '',
+        styles["container"],
+        direction === "vertical" ? styles["container-vertical"] : "",
+        direction === "horizontal" ? styles["container-horizontal"] : "",
+        className ?? ""
       )}
       {...rest}
     >
@@ -29,14 +29,14 @@ export function ContainerFixed(props: ContainerProps) {
   return (
     <div
       className={clsx(
-        `${styles['container-fixed']} container-fixed`,
-        direction === 'vertical'
-          ? `${styles['container']} ${styles['container-vertical']}`
-          : '',
-        direction === 'horizontal'
-          ? `${styles['container']} ${styles['container-horizontal']}`
-          : '',
-        className ?? '',
+        `${styles["container-fixed"]} container-fixed`,
+        direction === "vertical"
+          ? `${styles["container"]} ${styles["container-vertical"]}`
+          : "",
+        direction === "horizontal"
+          ? `${styles["container"]} ${styles["container-horizontal"]}`
+          : "",
+        className ?? ""
       )}
       {...rest}
     >
@@ -48,48 +48,48 @@ export function ContainerFixed(props: ContainerProps) {
 export function ContainerShrink(
   props: {
     absolute?: boolean;
-    overflowY?: 'hidden' | 'auto';
-  } & ContainerProps,
+    overflowY?: "hidden" | "auto";
+  } & ContainerProps
 ) {
   const { direction, className, children, absolute, overflowY, ...rest } =
     props;
 
   const containerClass = clsx(
-    `${styles['container-shrink']} container-shrink`,
+    `${styles["container-shrink"]} container-shrink`,
     absolute
-      ? ['relative']
+      ? ["relative"]
       : [
-          direction === 'vertical'
-            ? `${styles['container']} ${styles['container-vertical']}`
-            : '',
-          direction === 'horizontal'
-            ? `${styles['container']} ${styles['container-horizontal']}`
-            : '',
-          overflowY === 'hidden' ? 'overflow-y-hidden' : '',
-          overflowY === 'auto' ? 'overflow-y-auto' : '',
+          direction === "vertical"
+            ? `${styles["container"]} ${styles["container-vertical"]}`
+            : "",
+          direction === "horizontal"
+            ? `${styles["container"]} ${styles["container-horizontal"]}`
+            : "",
         ],
-    className ?? '',
+    className ?? ""
   );
 
   return absolute ? (
     <div className={containerClass} {...rest}>
       <div
-        className={clsx('absolute w-full h-full', [
-          direction === 'vertical'
-            ? `${styles['container']} ${styles['container-vertical']}`
-            : '',
-          direction === 'horizontal'
-            ? `${styles['container']} ${styles['container-horizontal']}`
-            : '',
-          overflowY ? `overflow-y-${overflowY}` : '',
+        className={clsx("absolute w-full h-full", [
+          direction === "vertical"
+            ? `${styles["container"]} ${styles["container-vertical"]}`
+            : "",
+          direction === "horizontal"
+            ? `${styles["container"]} ${styles["container-horizontal"]}`
+            : "",
         ])}
+        style={{
+          overflowY,
+        }}
       >
         {children}
       </div>
     </div>
   ) : (
-    <div className={containerClass} {...rest}>
+    <div className={containerClass} style={{ overflowY }} {...rest}>
       {children}
     </div>
   );
-} 
+}
